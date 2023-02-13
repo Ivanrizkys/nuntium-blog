d60c7ed491a286f4a58e5f95622a44ff493db7cf
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-02-13 20:35:49 +0700

    style: formating code

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,17 +7,17 @@ import SecondFeaturePost from '@/components/organism/SecondFeaturePost'
 import OtherPostSection from '@/components/organism/OtherPostSection'
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Layout>
-				<FirstFeaturePost />
-				<PostRecomendation />
-				<SecondFeaturePost />
-				<OtherPostSection />
-				<div className='mb-20'></div>
-			</Layout>
-		</>
-	)
+  return (
+    <>
+      <Layout>
+        <FirstFeaturePost />
+        <PostRecomendation />
+        <SecondFeaturePost />
+        <OtherPostSection />
+        <div className="mb-20"></div>
+      </Layout>
+    </>
+  )
 }
 
 export default Home
