c8493ce315e81c113c0579bceec49285bc00771b
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-02-14 18:08:42 +0700

    fix: build error

--- pages/tags.tsx
+++ pages/tags.tsx
@@ -10,12 +10,12 @@ const Tags: NextPage = () => {
       <div className="mt-[194px] flex flex-col items-center">
         <MainSearch />
         <div className="mt-[59px] w-11/12 md:w-full flex flex-wrap justify-center gap-y-6 gap-x-[15px] md:p-3">
-          {tags.map((tag, index) => (
+          {tags?.map((tag, index) => (
             <button
               key={index}
               className="rounded-[50px] border-1 border-solid border-black py-[8px] sm:py-[10px] px-[23px] sm:px-[25px] font-libre text-sm sm:text-base text-black"
             >
-              #{tag.toLowerCase().replaceAll(' ', '')}
+              #{tag?.toLowerCase().replaceAll(' ', '')}
             </button>
           ))}
         </div>
