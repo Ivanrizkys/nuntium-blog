c635d92bfbb5886f49772515f6d1cf480df1dc12
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-02-20 14:20:41 +0700

    feat: adding dummy slug

--- dummy/editor_picks.ts
+++ dummy/editor_picks.ts
@@ -1,42 +1,49 @@
 interface EditorPickType {
-    image: string,
-    category: string,
-    title: string,
-    author: string,
-    date: string,
-    time_read: string,
-    preview: string
+  image: string
+  category: string
+  title: string
+  author: string
+  date: string
+  time_read: string
+  preview: string
+  slug: string
 }
 
 let editorPicks: EditorPickType[]
 editorPicks = [
-    {
-        image: "/image/editor-pick-1.webp",
-        category: "MINIMALISM",
-        title: "Culpa sit Laboris Exercitation ea Fugiat",
-        author: "Leslie Pena",
-        date: "April 25, 2012",
-        time_read: "(6 mins read)",
-        preview: "Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit. Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo ullamco labore et esse."
-    },
-    {
-        image: "/image/editor-pick-2.webp",
-        category: "TECHNOLOGY",
-        title: "Amet non Ex Officia nulla Cupidatat",
-        author: "Jacob Henry",
-        date: "September 27, 2017",
-        time_read: "(8 mins read)",
-        preview: "Sint anim Lorem aute duis Lorem incididunt. Nulla nostrud irure id ipsum aute excepteur duis sint. Do occaecat sit dolor magna esse. Mollit incididunt cillum consectetur fugiat adipisicing dolor est id minim amet cillum esse Lorem. Deserunt non duis excepteur aliqua duis eu reprehenderit."
-    },
-    {
-        image: "/image/editor-pick-3.webp",
-        category: "ARTICLE",
-        title: "Dolor ex Tempor Cotur Culpa li",
-        author: "Darrell Lane",
-        date: "May 16, 2017",
-        time_read: " (3 mins read)",
-        preview: "Incididunt sint nulla eu adipisicing eu reprehenderit proident consequat est. Nisi consectetur incididunt mollit officia ut cupidatat laborum proident eu velit Lorem deserunt deserunt voluptate. Proident nostrud laborum reprehenderit culpa irure id labore do ad do minim nisi. Incididunt ex."
-    },
+  {
+    image: '/image/editor-pick-1.webp',
+    category: 'MINIMALISM',
+    title: 'Culpa sit Laboris Exercitation ea Fugiat',
+    author: 'Leslie Pena',
+    date: 'April 25, 2012',
+    time_read: '(6 mins read)',
+    preview:
+      'Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit. Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo ullamco labore et esse.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
+  {
+    image: '/image/editor-pick-2.webp',
+    category: 'TECHNOLOGY',
+    title: 'Amet non Ex Officia nulla Cupidatat',
+    author: 'Jacob Henry',
+    date: 'September 27, 2017',
+    time_read: '(8 mins read)',
+    preview:
+      'Sint anim Lorem aute duis Lorem incididunt. Nulla nostrud irure id ipsum aute excepteur duis sint. Do occaecat sit dolor magna esse. Mollit incididunt cillum consectetur fugiat adipisicing dolor est id minim amet cillum esse Lorem. Deserunt non duis excepteur aliqua duis eu reprehenderit.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
+  {
+    image: '/image/editor-pick-3.webp',
+    category: 'ARTICLE',
+    title: 'Dolor ex Tempor Cotur Culpa li',
+    author: 'Darrell Lane',
+    date: 'May 16, 2017',
+    time_read: ' (3 mins read)',
+    preview:
+      'Incididunt sint nulla eu adipisicing eu reprehenderit proident consequat est. Nisi consectetur incididunt mollit officia ut cupidatat laborum proident eu velit Lorem deserunt deserunt voluptate. Proident nostrud laborum reprehenderit culpa irure id labore do ad do minim nisi. Incididunt ex.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
 ]
 
 export default editorPicks
--- dummy/other_card.ts
+++ dummy/other_card.ts
@@ -1,47 +1,55 @@
 interface OhterCardType {
-    image: string,
-    category: string,
-    title: string,
-    author: string,
-    date: string,
-    preview: string
+  image: string
+  category: string
+  title: string
+  author: string
+  date: string
+  preview: string
+  slug: string
 }
 
 let ohterCard: OhterCardType[]
 
 ohterCard = [
-    {
-        image: "/image/other-post-1.webp",
-        category: "NATURE",
-        title: "Tempor deserunt Sunt Qui",
-        author: "Connie Robertson",
-        date: "November 3, 2012",
-        preview: "Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla tempor aute reprehenderit ut dolore mollit nisi consequat excepteur ex officia pariatur irure."
-    },
-    {
-        image: "/image/other-post-2.webp",
-        category: "PLANTS",
-        title: "Occaecat Anim eu Qui",
-        author: "Eleanor Williamson",
-        date: "September 28, 2014",
-        preview: "Qui ipsum consectetur ad incididunt et aliquip exercitation laboris nisi reprehenderit. Et excepteur commodo esse enim ea aliqua officia reprehenderit."
-    },
-    {
-        image: "/image/other-post-3.webp",
-        category: "FOOD",
-        title: "Fugiat Incididunt Voluptate Consequat",
-        author: "Bessie Watson",
-        date: "June 13, 2012",
-        preview: "Mollit ea culpa ipsum pariatur eiusmod. Irure et."
-    },
-    {
-        image: "/image/other-post-4.webp",
-        category: "COLORFUL",
-        title: "Occaecat Anim eu Qui",
-        author: "Ted Simmmons",
-        date: "June 21, 2015",
-        preview: "Amet ipsum occaecat aliqua aute nisi laboris nostrud culpa est do. Aliqua esse velit in excepteur esse qui voluptate laboris adipisicing qui irure elit amet excepteur."
-    },
+  {
+    image: '/image/other-post-1.webp',
+    category: 'NATURE',
+    title: 'Tempor deserunt Sunt Qui',
+    author: 'Connie Robertson',
+    date: 'November 3, 2012',
+    preview:
+      'Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla tempor aute reprehenderit ut dolore mollit nisi consequat excepteur ex officia pariatur irure.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
+  {
+    image: '/image/other-post-2.webp',
+    category: 'PLANTS',
+    title: 'Occaecat Anim eu Qui',
+    author: 'Eleanor Williamson',
+    date: 'September 28, 2014',
+    preview:
+      'Qui ipsum consectetur ad incididunt et aliquip exercitation laboris nisi reprehenderit. Et excepteur commodo esse enim ea aliqua officia reprehenderit.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
+  {
+    image: '/image/other-post-3.webp',
+    category: 'FOOD',
+    title: 'Fugiat Incididunt Voluptate Consequat',
+    author: 'Bessie Watson',
+    date: 'June 13, 2012',
+    preview: 'Mollit ea culpa ipsum pariatur eiusmod. Irure et.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
+  {
+    image: '/image/other-post-4.webp',
+    category: 'COLORFUL',
+    title: 'Occaecat Anim eu Qui',
+    author: 'Ted Simmmons',
+    date: 'June 21, 2015',
+    preview:
+      'Amet ipsum occaecat aliqua aute nisi laboris nostrud culpa est do. Aliqua esse velit in excepteur esse qui voluptate laboris adipisicing qui irure elit amet excepteur.',
+    slug: '/the-worlds-most-dangerous-technology-ever-made',
+  },
 ]
 
 export default ohterCard
