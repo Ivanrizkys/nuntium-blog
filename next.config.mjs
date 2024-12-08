0c5fe2c64ac031d994c7a38456d4b69ab813d109
Ivanrizkys <ivnrzkys@gmail.com>
2024-12-08 18:15:07 +0700

    chore: adding plaiceholder plugins

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withPlaiceholder from "@plaiceholder/next";
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
+  reactStrictMode: true,
+};
+ 
+export default withPlaiceholder(config);
